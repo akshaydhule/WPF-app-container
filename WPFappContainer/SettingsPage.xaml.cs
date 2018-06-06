d10c4f8dd3faf3750ddde146c3871556cbc495f2
Akshay Dhule <akshaydhule9@gmail.com>
2018-06-06 14:28:04 -0700

    Update SettingsPage.xaml.cs

--- WPFappContainer/SettingsPage.xaml.cs
+++ WPFappContainer/SettingsPage.xaml.cs
@@ -33,55 +33,6 @@ namespace DemoUI
             this.DataContext = new ConfigurationControl();
         }
 
-        /*
-                /// <summary>
-                /// Load custom setting page
-                /// </summary>
-                public void LoadExternalXaml()
-                {
-                    if (File.Exists(@"CustomSettings.xaml"))
-                    {
-                        using (FileStream stream = new FileStream(@"C:\CustomSettings.xaml", FileMode.Open))
-                        {
-                            this.Content = XamlReader.Load(stream);
-                        }
-                    }
-                }
-
-                /// <summary>
-                /// Load custom setting page
-                /// </summary>
-                public void SaveCustomXaml()
-                {
-                    using (FileStream stream = new FileStream(@"CustomSettings.xaml", FileMode.Create))
-                    {
-                        XamlWriter.Save(this.Content, stream);
-                    }
-                }
-
-                private void SubmitButton_Click(object sender, RoutedEventArgs e)
-                {
-                    //populate the HashSet for all Toggle values
-                    MessageBoxResult result = MessageBox.Show("Are you sure you want to save the custom settings?", "Notification", MessageBoxButton.OKCancel, MessageBoxImage.Question);
-                    switch (result)
-
-                    {
-                        //Accept save settings 
-                        case MessageBoxResult.OK:
-                            //Function call to iterate through list of settings to collect Trigger On/Off values
-                            //Setupdate();
-                            SaveCustomXaml();
-                            //Function call to save list of custom triggers in a file
-                            //SaveCustomTriggers();
-
-                            break;
-
-                        //Cancel save settings
-                        case MessageBoxResult.Cancel:
-                            MessageBox.Show("You pressed Cancel button!");
-                            break;
-
-                    }
-                }*/
+  
     }
 }
